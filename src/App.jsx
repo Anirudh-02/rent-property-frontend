cfe4fdae7ad82aca57a8521815b9a00228189044
Anirudh <anirudhshukla02@gmail.com>
2022-11-13 00:56:39 +0530

    added dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import Properties from './components/Properties'
 import propertyData from './data/data.json'
 
 function App() {
+  const [theme, setTheme] = useState('light')
   const [availableProperties, setAvailableProperties] = useState(propertyData)
   const [location, setLocation] = useState("")
   const [price, setPrice] = useState("")
   const [when, setWhen] = useState("")
   const [type, setType] = useState("")
+
+  function changeTheme() {
+    theme == 'light' ? setTheme('dark') : setTheme('light')
+  }
 
   function setFilterOptions(filterType, value) {
     if (filterType == 'location') {
@@ -30,14 +35,13 @@ function App() {
     setAvailableProperties(
       propertyData.filter((property) => {
         if (
-          (location ? property.city == location : 1) && 
+          (location ? property.city == location : 1) &&
           (price ? (property.price < price && property.price >= (price - 1000000)) : 1) &&
           (type ? property.type == type.toLowerCase() : 1) &&
           (when ? new Date(when).getTime() <= new Date(property.moveInDate).getTime() : 1)
-        )
-          {
-            return property
-          }
+        ) {
+          return property
+        }
       })
     )
   }
@@ -53,10 +57,12 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Navbar/>
-      <Filters location={location} type={type} when={when} price={price} clearFilterOptions={clearFilterOptions} setFilterOptions={setFilterOptions} filterProperties={filterProperties}/>
-      <Properties availableProperties={availableProperties}/>
+    <div className={`themeWrapper ${theme}`}>
+      <div className={`App dark:bg-zinc-800`}>
+        <Navbar theme={theme} changeTheme={changeTheme} />
+        <Filters location={location} type={type} when={when} price={price} clearFilterOptions={clearFilterOptions} setFilterOptions={setFilterOptions} filterProperties={filterProperties} />
+        <Properties availableProperties={availableProperties} />
+      </div>
     </div>
   )
 }
